2d32918e193b5ef545dda91012b57410f0ad6918
ahmed shaheen <ahmed.gamal2016@feps.edu.eg>
2022-05-24 12:35:17 +0200

    IP checker done

--- IP_checker.sh
+++ IP_checker.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then 
+    echo "privide exactly one argument  $0 IP"
+    exit 257
+fi
+
+IP=$1
+REGEX="[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+echo "you entered IP $IP"
+
+
+if ! [[ $IP =~ $REGEX ]]; then
+    echo "please enter a right IP like  [0-255].[0-255].[0-255].[0-255] "
+    exit 2
+
+fi
+
+IP=${BASH_REMATCH[0]}
+
+ping -c1 $IP 1> /dev/null
+
+
+if [ $? -ne 0 ]; then
+echo "IP $IP  is dead "
+else
+echo "IP $IP   is alive "
+fi
